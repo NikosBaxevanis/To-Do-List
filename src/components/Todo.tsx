edde7827d48f512461431059de5b88143889e124
NikosBaxevanis <baxemb@gmail.com>
2025-06-10 19:55:26 +0300

    Added inputRef

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useReducer} from 'react';
+import {useEffect, useReducer , useRef} from 'react';
 import TodoForm from "./TodoForm.tsx";
 import TodoList from "./TodoList.tsx";
 import type { TodoProps, Action} from "../type.ts"
@@ -46,24 +46,30 @@ const Todo = () =>{
     // console.log(todos);
     const [todos, dispatch] = useReducer(todoReducer, [], getInitialTodos);
 
+    const inputRef = useRef<HTMLInputElement>(null);
+
     const totalTasks: number = todos.length;
     const completedTasks: number = todos.filter(t => t.completed).length;
     const activeTasks : number = totalTasks - completedTasks;
+
+    const handleClearAll = () => {
+        dispatch({ type: "CLEAR_ALL" });
+        inputRef.current?.focus();
+    }
 
     useEffect(() => {
         localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos])
 
-
-    const handleClearAll = () => {
-        dispatch({ type: "CLEAR_ALL" });
-    }
+    useEffect(() => {
+        inputRef.current?.focus();
+    },[]);
 
     return (
         <>
             <div className="max-w-sm mx-auto p-6">
                 <h1 className="text-center text-2xl mb-4">To-Do List</h1>
-                <TodoForm dispatch={dispatch} />
+                <TodoForm dispatch={dispatch} inputRef={inputRef} />
                 <TodoList todos={todos} dispatch={dispatch} />
 
 
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
-import type { TodoFormProps } from "../types.ts";
+import {useEffect, useState} from "react";
+import type { TodoFormProps } from "../type.ts";
 
-const TodoForm = ({ dispatch }: TodoFormProps) => {
+const TodoForm = ({ dispatch, inputRef }: TodoFormProps) => {
 
     const [text, setText] = useState("");
+
+    //const inputRef = useRef<HTMLInputElement>(null);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setText(e.target.value);
@@ -14,8 +16,11 @@ const TodoForm = ({ dispatch }: TodoFormProps) => {
         if (text.trim() !== "") {
             dispatch({type: "ADD", payload: text});
             setText("");
+            inputRef.current?.focus();
         }
     };
+
+
 
     return (
         <>
@@ -24,6 +29,7 @@ const TodoForm = ({ dispatch }: TodoFormProps) => {
                 onSubmit={handleSubmit}
             >
                 <input
+                    ref={inputRef}
                     type="text"
                     value={text}
                     onChange={handleChange}
